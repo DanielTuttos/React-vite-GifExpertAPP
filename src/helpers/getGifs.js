80ed00a07be35f85efc729ee83287460552ce184
Daniel Romero AG <tutorialesdanielromero@gmail.com>
2022-06-20 14:58:29 -0500

    Creating the squeleton of the app

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+
+export const getGifs = async (category) => {
+    const api_key = 'rj89M3NK3HUWvUNZWPjM2x0rcsARc5D3'
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=20`;
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => (
+        {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    ))
+
+    console.log(gifs);
+
+}
