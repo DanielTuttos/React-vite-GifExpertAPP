465703825b3ba0dd945dfaf52db522f7c1c1959c
Daniel Romero AG <tutorialesdanielromero@gmail.com>
2022-06-24 11:39:21 -0500

    ending tests in GifsItem component

--- src/tests/components/GifsItem.test.jsx
+++ src/tests/components/GifsItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifsItem } from "../../components/";
 
 describe('Tests in <GifsItem />', () => {
@@ -9,6 +9,21 @@ describe('Tests in <GifsItem />', () => {
     test('must have match with snapshot', () => {
         const { container } = render(<GifsItem title={title} url={url} />)
         expect(container).toMatchSnapshot();
+    });
+
+    test('It must show the url with the indicated alt', () => {
+        render(<GifsItem title={title} url={url} />);
+        // screen.debug();
+        // expect(screen.getByRole('img').src).toBe(url);
+        // expect(screen.getByRole('img').alt).toBe(title);
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    })
+
+    test('must show the title in the component', () => {
+        render(<GifsItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
     });
 
 })
